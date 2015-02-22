89f9e89baf6edadcda7377b4e5a931e237fd74f9
Juvenn Woo <machese@gmail.com>
2015-02-22 10:51:00 +0800

    CTCI: add answers for string duplication, reverse etc

--- src/katas/clj/ctci.clj
+++ src/katas/clj/ctci.clj
@@ -2,15 +2,47 @@
   ^{:doc "Solutions to the book Cracking the Coding Interview"}
   katas.clj.ctci)
 
-(defn
-  ^{:qn "1.1"
-    :doc "Given a string, test if charaters are unique."}
-  chars-unique? [s]
-  (loop [m     {}
-         chars s]
+(defn chars-unique?
+  "Test if characters in string are unique."
+  ^{:qn "1.1"}
+  [s]
+  (loop [counts {}
+         chars  s]
     (let [c   (first chars)
-          cnt (get m c 0)]
+          cnt (get counts c 0)]
       (cond
         (nil? c)  true
         (> cnt 0) false
-        :else (recur (assoc m c 1) (rest chars))))))
+        :else (recur (assoc counts c (inc cnt)) (rest chars))))))
+
+(defn reverse-str
+  "Reverse c-style string, including null character."
+  ^{:qn "1.2"}
+  [s]
+  (loop [res ()
+         chars s]
+    (if (empty? chars) (apply str res)
+      (recur (conj res (first chars))
+             (rest chars)))))
+
+(defn remove-duplicate
+  "Remove dulicate characters in a string."
+  ^{:an "1.3"}
+  [s]
+  (loop [chars  s
+         res    []
+         counts {}]
+    (let [c (first chars)
+          cnt (get counts c 0)]
+      (if (nil? c) (apply str res)
+        (recur (rest chars)
+               (if (> cnt 0) res (conj res c))
+               (assoc counts c (inc cnt)))))))
+
+(defn anagrams?
+  "Are two strings anagrams?"
+  ^{:an "1.4"}
+  [s1 s2]
+  (if (= (count s1) (count s2))
+    (= s1 (reverse-str s2))
+    false))
--- test/katas/clj/ctci_test.clj
+++ test/katas/clj/ctci_test.clj
@@ -12,3 +12,53 @@
     (is (= false (ctci/chars-unique? "hell")))
     (is (= false (ctci/chars-unique? "hello")))
     ))
+
+(deftest test-reverse-str
+  ^{:qn "1.2"}
+  (testing "it should return empty string"
+    (is (= "" (ctci/reverse-str ""))))
+  (testing "it should reverse small strings"
+    (is (= "h" (ctci/reverse-str "h")))
+    (is (= "he" (ctci/reverse-str "eh")))
+    (is (= "hel" (ctci/reverse-str "leh")))
+    (is (= "hell" (ctci/reverse-str "lleh")))
+    (is (= "hello" (ctci/reverse-str "olleh")))
+    ))
+
+(deftest test-remove-duplicate
+  ^{:qn "1.3"}
+  (testing "it should return empty string"
+    (is (= "" (ctci/remove-duplicate ""))))
+  (testing "it should handle small strings"
+    (is (= "h" (ctci/remove-duplicate "h")))
+    (is (= "he" (ctci/remove-duplicate "he")))
+    (is (= "hel" (ctci/remove-duplicate "hel")))
+    (is (= "hel" (ctci/remove-duplicate "hell")))
+    (is (= "oleh" (ctci/remove-duplicate "olleh")))
+    (is (= "helo" (ctci/remove-duplicate
+                    "hellohellohellohello")))
+    (is (= "helo" (ctci/remove-duplicate "hello")))))
+
+(deftest test-anagrams?
+  ^{:qn "1.4"}
+  (testing "two empty strings should return true"
+    (is (= true (ctci/anagrams? "" "")))
+    )
+  (testing "strings with different length should return false"
+    (are [s1 s2] (= false (ctci/anagrams? s1 s2))
+         "h"     ""
+         "he"    "h"
+         "hel"   "he"
+         "hell"  "hel"
+         "hello" "hell"
+         ))
+  (testing "string and its reverse should return true"
+    (are [s] (ctci/anagrams? s (apply str (reverse s)))
+         "h"
+         "he"
+         "hel"
+         "hell"
+         "hello"
+         ))
+  )
+
