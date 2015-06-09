5633e5788a29fea62111cbed4ccd86bfa470bf0e
Juvenn Woo <machese@gmail.com>
2015-06-09 15:57:21 +0800

    LeetCode add inorder traversal to TreeNode #java

--- src/katas/java/LeetCode/TreeNode.java
+++ src/katas/java/LeetCode/TreeNode.java
@@ -1,9 +1,24 @@
 
 package katas.java;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
     public TreeNode (int n) { val = n; }
+
+    public List<Integer> inorder() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        inorderCollect(list);
+        return list;
+    }
+
+    public void inorderCollect(List<Integer> list) {
+        if (left != null) left.inorderCollect(list);
+        list.add(val);
+        if (right != null) right.inorderCollect(list);
+    }
 }
