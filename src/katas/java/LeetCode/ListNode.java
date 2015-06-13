93b7889788efd7a4cecb542de4bea9bd719d1959
Juvenn Woo <machese@gmail.com>
2015-06-13 13:45:01 +0800

    LeetCode add utility methods to ListNode and TreeNode #java

--- src/katas/java/LeetCode/ListNode.java
+++ src/katas/java/LeetCode/ListNode.java
@@ -1,8 +1,17 @@
 
 package katas.java;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ListNode {
     public int val;
     public ListNode next;
     public ListNode (int x) { val = x; }
+
+    public List<Integer> seq() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (ListNode x = this; x != null; x = x.next)
+            list.add(x.val);
+        return list;
+    }
 }
--- src/katas/java/LeetCode/TreeNode.java
+++ src/katas/java/LeetCode/TreeNode.java
@@ -10,15 +10,61 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode (int n) { val = n; }
 
+    public int size() {
+        return size(this);
+    }
+
+    public int size(TreeNode x) {
+        if (x == null) return 0;
+        return size(x.left) + size(x.right) + 1;
+    }
+
+    public int height() {
+        return height(this);
+    }
+
+    public int height(TreeNode x) {
+        if (x == null) return 0;
+        return Math.max(height(x.left), height(x.right)) + 1;
+    }
+
     public List<Integer> inorder() {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        inorderCollect(list);
+        inorder(this, list);
         return list;
     }
 
-    public void inorderCollect(List<Integer> list) {
-        if (left != null) left.inorderCollect(list);
-        list.add(val);
-        if (right != null) right.inorderCollect(list);
+    public void inorder(TreeNode x, List<Integer> list) {
+        if (x == null) return;
+        inorder(x.left, list);
+        list.add(x.val);
+        inorder(x.right, list);
     }
+
+    public List<Integer> preorder() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        preorder(this, list);
+        return list;
+    }
+
+    private void preorder(TreeNode x, List<Integer> list) {
+        if (x == null) return;
+        list.add(val);
+        preorder(x.left, list);
+        preorder(x.right, list);
+    }
+
+    public List<Integer> postorder() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        postorder(this, list);
+        return list;
+    }
+
+    private void postorder(TreeNode x, List<Integer> list) {
+        if (x == null) return;
+        postorder(x.left, list);
+        postorder(x.right, list);
+        list.add(x.val);
+    }
+
 }
